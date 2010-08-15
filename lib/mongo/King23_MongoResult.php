f673d03e9633840369368961b654abca71d4a7fd
Peter Petermann <ppetermann80@googlemail.com>
2010-08-15 09:04:03 +0000

    added license header, added count method to result class

--- lib/mongo/King23_MongoResult.php
+++ lib/mongo/King23_MongoResult.php
@@ -1,5 +1,31 @@
 <?php
-class King23_MongoResult implements Iterator
+/*
+ MIT License
+ Copyright (c) 2010 Peter Petermann
+
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+class King23_MongoResult implements Iterator, Countable
 {
 
     /**
@@ -62,7 +88,7 @@ class King23_MongoResult implements Iterator
     /**
      * Iterator::current
      * return current specific obect
-     * @return King23_MongoObect
+     * @return King23_MongoObject
      */
     public function current()
     {
@@ -71,5 +97,13 @@ class King23_MongoResult implements Iterator
         $k23doc = new $class();
         $k23doc->_loadFromArray($doc);
         return $k23doc;
+    }
+    
+    /**
+    * count method on the cursor, allows to get result count
+    */ 
+    public function count()
+    {
+        return $this->_cursor->count();
     }
 }
