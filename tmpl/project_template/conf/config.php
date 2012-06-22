b0486db7d88b0bcb84c9e1566fef6e5c5518307e
Peter Petermann <ppetermann80@googlemail.com>
2012-06-23 01:43:12 +0200

    working on composer support

--- tmpl/project_template/conf/config.php
+++ tmpl/project_template/conf/config.php
@@ -51,8 +51,10 @@ $reg->sith = new TemplateEnviron(array(
     'autoEscape'             => false,
 ));*/
 
+// composer autoload
+require_once "vendor/autoload.php";
+
 // Twig Template configuration
-require_once(APP_PATH . "/lib/King23/external/Twig/lib/Twig/Autoloader.php");
 Twig_Autoloader::register();
 $reg->twig = new Twig_Environment(new Twig_Loader_Filesystem(APP_PATH ."/templates"), array(
     "cache" => APP_PATH . "/templates_c"
