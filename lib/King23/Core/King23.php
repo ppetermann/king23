e7c820adaa4ba63cf921e5a693590b71c147717b
Peter Petermann <ppetermann80@googlemail.com>
2013-06-05 22:50:52 +0200

    version bump

--- lib/King23/Core/King23.php
+++ lib/King23/Core/King23.php
@@ -31,7 +31,7 @@ namespace King23\Core;
  */
 abstract class King23
 {
-    const Version = "0.1.7";
+    const Version = "0.1.8";
     const Description = "King23 is a lightweight MVC Framework";
     const Authors = "Peter Petermann <ppeterman80@googlemail.com>";
 }
