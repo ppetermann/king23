1448f7b758e4c62c6b462615a86ff46b90f9b4d2
Peter Petermann <peter.petermann@gameforge.com>
2015-08-23 12:07:22 +0200

    Release of new version 1.0.0-alpha

--- src/King23/Core/King23.php
+++ src/King23/Core/King23.php
@@ -32,7 +32,7 @@ namespace King23\Core;
  */
 abstract class King23
 {
-    const VERSION = "0.9.1";
+    const VERSION = "1.0.0-alpha";
     const DESCRIPTION = "King23 is a lightweight MVC Framework";
     const AUTHORS = "Peter Petermann <ppeterman80@googlemail.com>";
 }
