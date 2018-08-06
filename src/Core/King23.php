0b5af65da0196d68b579082b7bd3fd8d47609274
Peter Petermann <ppetermann80@googlemail.com>
2018-08-06 14:25:42 +0200

    Release of new version 3.0.0-alpha

--- src/Core/King23.php
+++ src/Core/King23.php
@@ -32,7 +32,7 @@ namespace King23\Core;
  */
 abstract class King23
 {
-    const VERSION = "2.0.0";
+    const VERSION = "3.0.0-alpha";
     const DESCRIPTION = "King23 is a lightweight MVC Framework";
     const AUTHORS = "Peter Petermann <ppeterman80@googlemail.com>";
 }
