3495b0d6a2ec44e3a527838c3fc8c1ee6584362f
Peter Petermann <ppetermann80@googlemail.com>
2018-09-09 16:26:06 +0200

    Release of new version 4.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 
 ## VERSION 4  TWIG CONTROLLER UPDATE
 
+ * Version **4.1** - new way of handling settings
+   * 2018-09-09 16:26  **4.1.0**  initial release
+
  * Version **4.0** - twig controller update
    * 2018-08-31 17:41  **4.0.0**  initial release
--- src/Core/King23.php
+++ src/Core/King23.php
@@ -32,7 +32,7 @@ namespace King23\Core;
  */
 abstract class King23
 {
-    const VERSION = "4.0.0";
+    const VERSION = "4.1.0";
     const DESCRIPTION = "King23 is a lightweight MVC Framework";
     const AUTHORS = "Peter Petermann <ppeterman80@googlemail.com>";
 }
